ed2bfdf6bb59263902a083f2a7b2087837ac3619
geoffhuntsgood <geoffhuntsgood@gmail.com>
2026-08-04 18:55:00 -0400

    remove parse

--- src/server.ts
+++ src/server.ts
@@ -37,7 +37,7 @@ app.get("/quiz-best-time/:category", (req: Request, res: Response) => {
 });
 
 app.post("/quiz-best-time", (req: Request, res: Response) => {
-  const body: BestTime = JSON.parse(req.body);
+  const body: BestTime = req.body;
   console.log(body.player_name);
 
   if (!body) {
