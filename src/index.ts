e3a6b11acb234284faf6a5f3542f7c1ae84915ef
geoffhuntsgood <geoffhuntsgood@gmail.com>
2026-09-22 20:56:31 -0400

    had to re-consolidate for now to make render happy

--- src/index.ts
+++
@@ -1,8 +0,0 @@
-import { type Express } from "express";
-import { setupServer } from "./server";
-
-const server: Express = setupServer();
-
-server.listen(3000, () => {
-  console.log("geoff-server up at port 3000");
-});
--- src/server.ts
+++ src/server.ts
@@ -1,78 +1,67 @@
 import cors from "cors";
-import "dotenv/config";
-import express, {
-  type Express,
-  json,
-  Request,
-  Response,
-  urlencoded
-} from "express";
-import { client } from "./db";
+import express, { json, Request, Response, urlencoded } from "express";
+import { Client } from "pg";
 import { BestTime } from "./types";
 
-export const setupServer: () => Express = () => {
-  const server = express();
-  server.use(cors({ origin: true }));
-  server.use(urlencoded());
-  server.use(json());
+const server = express();
+server.use(cors({ origin: true }));
+server.use(urlencoded());
+server.use(json());
 
-  const getQuery = (category: string) => {
-    return `
+const pgClient = new Client({
+  host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT),
+  database: process.env.DB_DATABASE,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
+
+server.get("/get-best-time/:category", async (req: Request, res: Response) => {
+  try {
+    await pgClient.connect();
+    const getTime = await pgClient.query(`
       SELECT json_agg(quiz_times) FROM quiz_times
-      WHERE category = ${category}
+      WHERE category = ${req.params.category}
       GROUP BY best_time ORDER BY best_time ASC LIMIT 1
-    `;
-  };
+    `);
+    if (getTime.rows.length > 0) {
+      return res.status(200).json(getTime.rows[0]);
+    } else {
+      return res.status(204).json({ msg: "No time found for this category." });
+    }
+  } catch (err) {
+    console.error(err);
+    return res.status(500).json({ msg: err });
+  } finally {
+    await pgClient.end();
+  }
+});
 
-  const postQuery = (best: BestTime) => {
-    return `
+server.post("/save-best-time", async (req: Request, res: Response) => {
+  const body: BestTime = req.body;
+
+  try {
+    await pgClient.connect();
+    await pgClient.query(`
       INSERT INTO quiz_times (player_name, category, best_time)
-      VALUES (${best.player_name}, ${best.category}, ${best.best_time})
+      VALUES (${body.player_name}, ${body.category}, ${body.best_time})
       ON CONFLICT (player_name, category) DO UPDATE
       SET best_time = LEAST(excluded.best_time, quiz_times.best_time)
-    `;
-  };
+    `);
+    return res.status(201).json({
+      msg: `Updated ${body.category} with ${body.best_time} for player ${body.player_name}`
+    });
+  } catch (err) {
+    console.error(err);
+    return res.status(500).json({ msg: err });
+  } finally {
+    await pgClient.end();
+  }
+});
 
-  server.get(
-    "/get-best-time/:category",
-    async (req: Request, res: Response) => {
-      const category = req.params.category;
-
-      try {
-        await client.connect();
-        const getTime = await client.query(getQuery(String(category)));
-        if (getTime.rows.length > 0) {
-          return res.status(200).json(getTime.rows[0]);
-        } else {
-          return res
-            .status(204)
-            .json({ msg: "No time found for this category." });
-        }
-      } catch (err) {
-        console.error(err);
-        return res.status(500).json({ msg: err });
-      } finally {
-        await client.end();
-      }
-    }
-  );
-
-  server.post("/save-best-time", async (req: Request, res: Response) => {
-    const body: BestTime = req.body;
-
-    try {
-      await client.connect();
-      await client.query(postQuery(body));
-      return res.status(201).json({
-        msg: `Updated ${body.category} with ${body.best_time} for player ${body.player_name}`
-      });
-    } catch (err) {
-      console.error(err);
-      return res.status(500).json({ msg: err });
-    } finally {
-      await client.end();
-    }
-  });
-
-  return server;
-};
+server.listen(3000, () => {
+  console.log("geoff-server up at port 3000");
+});
